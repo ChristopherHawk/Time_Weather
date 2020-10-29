f5264ac7597955a7abd42eae3728cbb6d8e93078
Nestor <nestorbarrazaotalvarez@hotmail.com>
2020-10-29 15:34:54 -0500

    Settings

--- index.js
+++ index.js
@@ -30,128 +30,152 @@ const Paris = document.getElementById('paris');
 const Quebec = document.getElementById('quebec');
 //--------------------------------------------------//
 
-
-
-//-----Reload-----//
-if(location.pathname){
-  let urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`;
-  let urlLyon = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`;
-  let urlParis = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`; 
-  let urlQuebec = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`; 
+//-----select-----//
+function select(Dir){  
   
-
   if(SelectCity.value === 'bogotá'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`
     BoxTop.style.backgroundImage = 'url(img/bogota.jpg)';
     bigNameCTY.style.color = '  rgb(197, 168, 4)';
 
   }
   if(SelectCity.value === 'medellín'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Medellin,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Medellin,co`
     BoxTop.style.backgroundImage = 'url(img/medellin.jpg)';
+
   }
   if(SelectCity.value === 'cali'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Cali,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Cali,co`
     BoxTop.style.backgroundImage = 'url(img/cali.jpg)';
   }
   if(SelectCity.value === 'barranquilla'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Barranquilla,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Barranquilla,co`
     BoxTop.style.backgroundImage = 'url(img/barranquilla.jpg)';
   }
   if(SelectCity.value === 'cúcuta'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Cucuta,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Cucuta,co`
     BoxTop.style.backgroundImage = 'url(img/cucuta.jpg)';
   }
   if(SelectCity.value === 'bucaramanga'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bucaramanga,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Bucaramanga,co`
     BoxTop.style.backgroundImage = 'url(img/bucaramanga.jpg)';
   }
   if(SelectCity.value === 'lyon'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr`
     BoxTop.style.backgroundImage = 'url(img/lyon2.jpg)';
     bigNameCTY.style.color = 'rgb(197, 65, 4)';
   }
   if(SelectCity.value === 'parís'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr`
     BoxTop.style.backgroundImage = 'url(img/paris2.jpg)';
     bigNameCTY.style.color = 'rgb(4, 120, 197)';
-
-  }
-  if(SelectCity.value === 'quebec'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+  }if(SelectCity.value === 'quebec'){    
+    Dir = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca`
     BoxTop.style.backgroundImage = 'url(img/quebec.jpg)';
     bigNameCTY.style.color = 'rgb(4, 197, 123)';
 
   }
-  getWeather(NameCT1, urlTop, divBogota, iconWeather, Temp1, HumidCity1);
-  getWeather(NameCT2, urlLyon, divLyon, iconWeather2, Temp2, HumidCity2);
-  getWeather(NameCT3, urlParis, divParis, iconWeather3, Temp3, HumidCity3);
-  getWeather(NameCT4, urlQuebec, divQuebec, iconWeather4, Temp4, HumidCity4);
+  
+}
+
+
+
+//-----Reload-----//
+if(location.pathname){
+  let urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`;
+  let urlLyon = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr`;
+  let urlParis = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr`; 
+  let urlQuebec = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca`; 
+  select(urlTop);    
+  select(urlLyon);
+  select(urlParis);
+  select(urlQuebec);
+  let español = '&lang=es'
+
+  let frances = '&lang=fr'
+
+  getWeather(NameCT1, urlTop, español, divBogota, iconWeather, Temp1, HumidCity1);
+  getWeather(NameCT2, urlLyon, español,  divLyon, iconWeather2, Temp2, HumidCity2);
+  getWeather(NameCT3, urlParis, español,  divParis, iconWeather3, Temp3, HumidCity3);
+  getWeather(NameCT4, urlQuebec, español, divQuebec, iconWeather4, Temp4, HumidCity4);
   bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
 
 }
 
 
 
-//-----[Bogotá]-----//
-Bogota.addEventListener('mouseover', function(){
-  let urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`;
 
+//-----Box Top [Bogotá - Default]-----//
+document.addEventListener('mouseover', function(){ 
+  
+  
   if(SelectCity.value === 'bogotá'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`
     BoxTop.style.backgroundImage = 'url(img/bogota.jpg)';
     bigNameCTY.style.color = '  rgb(197, 168, 4)';
+
   }
   if(SelectCity.value === 'medellín'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Medellin,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Medellin,co`
     BoxTop.style.backgroundImage = 'url(img/medellin.jpg)';
+
   }
   if(SelectCity.value === 'cali'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Cali,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Cali,co`
     BoxTop.style.backgroundImage = 'url(img/cali.jpg)';
   }
   if(SelectCity.value === 'barranquilla'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Barranquilla,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Barranquilla,co`
     BoxTop.style.backgroundImage = 'url(img/barranquilla.jpg)';
   }
   if(SelectCity.value === 'cúcuta'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Cucuta,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Cucuta,co`
     BoxTop.style.backgroundImage = 'url(img/cucuta.jpg)';
   }
   if(SelectCity.value === 'bucaramanga'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bucaramanga,co&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bucaramanga,co`
     BoxTop.style.backgroundImage = 'url(img/bucaramanga.jpg)';
   }
   if(SelectCity.value === 'lyon'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr`
     BoxTop.style.backgroundImage = 'url(img/lyon2.jpg)';
     bigNameCTY.style.color = 'rgb(197, 65, 4)';
   }
   if(SelectCity.value === 'parís'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr`
     BoxTop.style.backgroundImage = 'url(img/paris2.jpg)';
     bigNameCTY.style.color = 'rgb(4, 120, 197)';
-  }if(SelectCity.value === 'quebec'){    
-    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`
+  }
+  if(SelectCity.value === 'quebec'){    
+    urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca`
     BoxTop.style.backgroundImage = 'url(img/quebec.jpg)';
     bigNameCTY.style.color = 'rgb(4, 197, 123)';
 
-  }
- 
-  getWeather(NameCT1, urlTop, divBogota, iconWeather, Temp1, HumidCity1);
-  Bogota.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
+  }  
+  Bogota.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)';
   bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
+  let español = '&lang=es'
+  let frances = '&lang=fr'  
+  getWeather(NameCT1, urlTop, español, divBogota, iconWeather, Temp1, HumidCity1);
+  
 });
 
 Bogota.addEventListener('mouseout', function(){  
+
   Bogota.style.boxShadow = ''
 });
 
+
+
+ 
+
+
+
+
+
 //-----Lyon-----//
 Lyon.addEventListener('mouseover', function(){
-  let urlLyon = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`;
-  getWeather(NameCT2, urlLyon, divLyon, iconWeather2, Temp2, HumidCity2);
-  Lyon.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
+    Lyon.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
 });
 Lyon.addEventListener('mouseout', function(){  
   Lyon.style.boxShadow = ''
@@ -159,9 +183,7 @@ Lyon.addEventListener('mouseout', function(){
 
 //-----París-----//
 Paris.addEventListener('mouseover', function(){
-  let urlParis = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`;
-  getWeather(NameCT3, urlParis, divParis, iconWeather3, Temp3, HumidCity3);
-  Paris.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
+ Paris.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
 });
 Paris.addEventListener('mouseout', function(){  
   Paris.style.boxShadow = ''
@@ -169,9 +191,7 @@ Paris.addEventListener('mouseout', function(){
 
 //-----Quebec-----//
 Quebec.addEventListener('mouseover', function(){
-  let urlQuebec = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca&APPID=b842860a5235b80c4f8e7a1b990abe91&lang=es`;
-  getWeather(NameCT4, urlQuebec, divQuebec, iconWeather4, Temp4, HumidCity4);
-  Paris.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
+  Quebec.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
 });
 Quebec.addEventListener('mouseout', function(){  
   Quebec.style.boxShadow = ''
@@ -179,11 +199,20 @@ Quebec.addEventListener('mouseout', function(){
 
 
 //-----Files [Weather]-----//
-function getWeather(nameCity, parameters, weather, icon, temp, humid){
+async function getWeather(nameCity, parameters, idiom, weather, icon, temp, humid){
 
   let Name_City = nameCity;
+
+  let API_KEY = '&APPID=b842860a5235b80c4f8e7a1b990abe91';
+
+  let Language = idiom;
+
+  
+  
+
+
   //---URL---//
-  let url = parameters;
+  let url = parameters + API_KEY + Language;
   //---WEATHER---//
   let result = weather;
   //---WEATHER ICON---//
@@ -193,9 +222,9 @@ function getWeather(nameCity, parameters, weather, icon, temp, humid){
   //---HUMIDITY---//
   let humidCity = humid
 
-  const api = new XMLHttpRequest();
-  api.open('GET', url, true);
-  api.send();
+  const api =  new XMLHttpRequest();
+  await api.open('GET', url, true);
+  await api.send();
   api.onreadystatechange = function () {
 
     if(this.status == 200 && this.readyState == 4){
@@ -203,8 +232,20 @@ function getWeather(nameCity, parameters, weather, icon, temp, humid){
       result.innerHTML = '';
       //Name City
       const Name = files.name;
-      const Country = files.sys.country;  
+      const Country = files.sys.country;
       Name_City.innerHTML = Name + ' - ' + Country;
+
+
+     // City Code Url
+      function capitalizeWords( val ) {
+  
+        return val.toLowerCase()
+                  .trim()
+                  .split(' ')
+                  .map( v => v[0].toUpperCase() + v.substr(1) )
+                  .join(' ');  
+      }
+      const cityCOde = capitalizeWords(Name) + ',' + Country.toLowerCase();  
 
       //Weather
       for(let item of files.weather){
