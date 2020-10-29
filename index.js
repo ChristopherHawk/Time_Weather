039c18bf7b9fc2313a5bf2ea88210300abcf17c7
Nestor <nestorbarrazaotalvarez@hotmail.com>
2020-10-29 16:32:31 -0500

    Language Select

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded',m);
 const btn = document.getElementById('btn');
 const BoxTop = document.getElementById('boxTop');
 const SelectCity = document.getElementById('selectCity');
+const LanguageSelect = document.getElementById('Language');
 const bigNameCTY = document.getElementById('bigNameCTY');
 const NameCT1 = document.getElementById('nameCT1');
 const NameCT2 = document.getElementById('nameCT2');
@@ -29,6 +30,7 @@ const Lyon = document.getElementById('lyon');
 const Paris = document.getElementById('paris');
 const Quebec = document.getElementById('quebec');
 //--------------------------------------------------//
+
 
 //-----select-----//
 function select(Dir){  
@@ -78,7 +80,24 @@ function select(Dir){
   
 }
 
+LanguageSelect.addEventListener('change', () =>{
+  let urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`;
+  let urlLyon = `http://api.openweathermap.org/data/2.5/weather?q=Lyon,fr`;
+  let urlParis = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr`; 
+  let urlQuebec = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca`; 
+  select(urlTop);    
+  select(urlLyon);
+  select(urlParis);
+  select(urlQuebec);
+  
+  getWeather(NameCT1, urlTop,  divBogota, iconWeather, Temp1, HumidCity1);
+  getWeather(NameCT2, urlLyon,  divLyon, iconWeather2, Temp2, HumidCity2);
+  getWeather(NameCT3, urlParis,  divParis, iconWeather3, Temp3, HumidCity3);
+  getWeather(NameCT4, urlQuebec, divQuebec, iconWeather4, Temp4, HumidCity4);
+  bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
 
+})
+//--------------------//
 
 //-----Reload-----//
 if(location.pathname){
@@ -90,14 +109,11 @@ if(location.pathname){
   select(urlLyon);
   select(urlParis);
   select(urlQuebec);
-  let español = '&lang=es'
-
-  let frances = '&lang=fr'
-
-  getWeather(NameCT1, urlTop, español, divBogota, iconWeather, Temp1, HumidCity1);
-  getWeather(NameCT2, urlLyon, español,  divLyon, iconWeather2, Temp2, HumidCity2);
-  getWeather(NameCT3, urlParis, español,  divParis, iconWeather3, Temp3, HumidCity3);
-  getWeather(NameCT4, urlQuebec, español, divQuebec, iconWeather4, Temp4, HumidCity4);
+  
+  getWeather(NameCT1, urlTop,  divBogota, iconWeather, Temp1, HumidCity1);
+  getWeather(NameCT2, urlLyon,  divLyon, iconWeather2, Temp2, HumidCity2);
+  getWeather(NameCT3, urlParis,  divParis, iconWeather3, Temp3, HumidCity3);
+  getWeather(NameCT4, urlQuebec, divQuebec, iconWeather4, Temp4, HumidCity4);
   bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
 
 }
@@ -106,8 +122,7 @@ if(location.pathname){
 
 
 //-----Box Top [Bogotá - Default]-----//
-SelectCity.addEventListener('mouseout', function(){ 
-  
+SelectCity.addEventListener('mouseout', function(){   
   
   if(SelectCity.value === 'bogotá'){    
     urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`
@@ -154,9 +169,8 @@ SelectCity.addEventListener('mouseout', function(){
   }  
   Bogota.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)';
   bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
-  let español = '&lang=es'
-  let frances = '&lang=fr'  
-  getWeather(NameCT1, urlTop, español, divBogota, iconWeather, Temp1, HumidCity1);
+   
+  getWeather(NameCT1, urlTop, divBogota, iconWeather, Temp1, HumidCity1);
   
 });
 
@@ -165,14 +179,6 @@ Bogota.addEventListener('mouseout', function(){
   Bogota.style.boxShadow = ''
 });
 
-
-
- 
-
-
-
-
-
 //-----Lyon-----//
 Lyon.addEventListener('mouseover', function(){
     Lyon.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
@@ -180,7 +186,6 @@ Lyon.addEventListener('mouseover', function(){
 Lyon.addEventListener('mouseout', function(){  
   Lyon.style.boxShadow = ''
 });
-
 //-----París-----//
 Paris.addEventListener('mouseover', function(){
  Paris.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
@@ -188,7 +193,6 @@ Paris.addEventListener('mouseover', function(){
 Paris.addEventListener('mouseout', function(){  
   Paris.style.boxShadow = ''
 });
-
 //-----Quebec-----//
 Quebec.addEventListener('mouseover', function(){
   Quebec.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'
@@ -199,18 +203,14 @@ Quebec.addEventListener('mouseout', function(){
 
 
 //-----Files [Weather]-----//
-async function getWeather(nameCity, parameters, idiom, weather, icon, temp, humid){
+function getWeather(nameCity, parameters,  weather, icon, temp, humid){
 
+  //---NAME CITY---//
   let Name_City = nameCity;
-
+  //---API KEY---//
   let API_KEY = '&APPID=b842860a5235b80c4f8e7a1b990abe91';
-
-  let Language = idiom;
-
-  
-  
-
-
+  //---LANGUAGE---//
+  let Language = LanguageSelect.value;
   //---URL---//
   let url = parameters + API_KEY + Language;
   //---WEATHER---//
@@ -223,8 +223,8 @@ async function getWeather(nameCity, parameters, idiom, weather, icon, temp, humi
   let humidCity = humid
 
   const api =  new XMLHttpRequest();
-  await api.open('GET', url, true);
-  await api.send();
+  api.open('GET', url, true);
+  api.send();
   api.onreadystatechange = function () {
 
     if(this.status == 200 && this.readyState == 4){
@@ -233,19 +233,7 @@ async function getWeather(nameCity, parameters, idiom, weather, icon, temp, humi
       //Name City
       const Name = files.name;
       const Country = files.sys.country;
-      Name_City.innerHTML = Name + ' - ' + Country;
-
-
-     // City Code Url
-      function capitalizeWords( val ) {
-  
-        return val.toLowerCase()
-                  .trim()
-                  .split(' ')
-                  .map( v => v[0].toUpperCase() + v.substr(1) )
-                  .join(' ');  
-      }
-      const cityCOde = capitalizeWords(Name) + ',' + Country.toLowerCase();  
+      Name_City.innerHTML = Name + ' - ' + Country;     
 
       //Weather
       for(let item of files.weather){
@@ -256,14 +244,27 @@ async function getWeather(nameCity, parameters, idiom, weather, icon, temp, humi
         image.style.width = '120px'
         image.style.height = '120px'       
       }      
-      //Temperature 
-      const temperature = files.main.temp;
-      tempCity.innerHTML = 'Temperatura: ' + temperature + ' K';
+     
+      if(LanguageSelect.value === '&lang=es'){
+        //Temperature 
+        const temperature = files.main.temp;
+        tempCity.innerHTML = 'Temperatura: ' + temperature + ' K';
 
-      //Humidity 
-      const humidity = files.main.humidity;
-      humidCity.innerHTML = 'Humedad: ' + humidity + '%';
-       
+        //Humidity 
+        const humidity = files.main.humidity;
+        humidCity.innerHTML = 'Humedad: ' + humidity + '%';
+      
+      }
+      if(LanguageSelect.value === '&lang=fr'){
+        //Temperature 
+        const temperature = files.main.temp;
+        tempCity.innerHTML = 'Température: ' + temperature + ' K';
+
+        //Humidity 
+        const humidity = files.main.humidity;
+        humidCity.innerHTML = 'Humidité: ' + humidity + '%';      
+      }
+     
                     
     }
   }
