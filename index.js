328e4fc74be01b47264b4c41e990c75db5dc5ba3
Nestor <nestorbarrazaotalvarez@hotmail.com>
2020-11-01 12:14:05 -0500

    Styles

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 window.addEventListener('DOMContentLoaded',m);
 const btn = document.getElementById('btn');
 const CloseSettings = document.getElementById('closeSettings');
+const ClockContainer = document.getElementById('clockContainer');
+const Preferences = document.getElementById('preferences');
 const BoxTop = document.getElementById('boxTop');
 const SelectCity = document.getElementById('selectCity');
 const LanguageSelect = document.getElementById('Language');
@@ -33,12 +35,12 @@ const Paris = document.getElementById('paris');
 const Quebec = document.getElementById('quebec');
 const CountryInput = document.getElementById('countryInput');
 const CityInput = document.getElementById('cityInput');
+const CityPicture1 = document.getElementById('cityPicture1');
+const CityPicture2 = document.getElementById('cityPicture2');
 const AddNewCity = document.getElementById('addNewCity');
-const NEWCARD = document.getElementById("NEWCARD"); 
-
 //--------------------------------------------------//
 
-//-----select-----//
+//-----Select-----//
 function select(Dir){  
   
   if(SelectCity.value === 'bogotá'){    
@@ -77,7 +79,8 @@ function select(Dir){
     Dir = `http://api.openweathermap.org/data/2.5/weather?q=Paris,fr`
     BoxTop.style.backgroundImage = 'url(img/paris2.jpg)';
     bigNameCTY.style.color = 'rgb(4, 120, 197)';
-  }if(SelectCity.value === 'quebec'){    
+  }
+  if(SelectCity.value === 'quebec'){    
     Dir = `http://api.openweathermap.org/data/2.5/weather?q=Quebec,ca`
     BoxTop.style.backgroundImage = 'url(img/quebec.jpg)';
     bigNameCTY.style.color = 'rgb(4, 197, 123)';
@@ -85,7 +88,6 @@ function select(Dir){
   }
   
 }
-
 //-------Language-------//
 LanguageSelect.addEventListener('change', () =>{
   let urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`;
@@ -103,7 +105,7 @@ LanguageSelect.addEventListener('change', () =>{
   getWeather(NameCT4, urlQuebec, divQuebec, iconWeather4, Temp4, HumidCity4);
   bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
 
-})
+});
 
 //-----Reload-----//
 if(location.pathname){    
@@ -122,9 +124,8 @@ if(location.pathname){
   bigNameCTY.innerHTML = SelectCity.value.toUpperCase();
 
 }
-
 //-----Box Top [Bogotá - Default]-----//
-SelectCity.addEventListener('mouseout', function(){   
+SelectCity.addEventListener('change', function(){   
   
   if(SelectCity.value === 'bogotá'){    
     urlTop = `http://api.openweathermap.org/data/2.5/weather?q=Bogota,co`
@@ -184,12 +185,11 @@ Idiom.addEventListener('mouseover', function(){
 Idiom.addEventListener('mouseout', function(){  
   Idiom.style.boxShadow = '';
   if(SelectCity.value === 'bogotá'){ 
-    BoxTop.style.backgroundImage = 'url(img/bogota.jpg)';   
+    BoxTop.style.backgroundImage = 'url(img/bogota.jpeg)';   
 
   }
   if(SelectCity.value === 'medellín'){    
     BoxTop.style.backgroundImage = 'url(img/medellin.jpg)';
-
   }
   if(SelectCity.value === 'cali'){ 
     BoxTop.style.backgroundImage = 'url(img/cali.jpg)';
@@ -234,18 +234,43 @@ AddNewCity.addEventListener('mouseover', function(){
   
 });
 AddNewCity.addEventListener('mouseout', function(){  
-  AddNewCity.style.boxShadow = '';  
+  AddNewCity.style.boxShadow = ''; 
+ 
+});
+ClockContainer.addEventListener('mouseover', function(){  
+  ClockContainer.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)';
+  ClockContainer.style.cursor = 'pointer'
+  BoxTop.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px #fff';
+});
+ClockContainer.addEventListener('mouseout', function(){  
+  ClockContainer.style.boxShadow = ''; 
+  BoxTop.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)';
+});
+Preferences.addEventListener('mouseover', function(){  
+  Preferences.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)';
+  Preferences.style.cursor = 'pointer'
+  BoxTop.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px #fff';
+});
+Preferences.addEventListener('mouseout', function(){  
+  Preferences.style.boxShadow = '';  
+  BoxTop.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)'; 
+});
+BoxTop.addEventListener('mouseout', function(){      
+  BoxTop.style.boxShadow = ''; 
+});
+BoxTop.addEventListener('mouseout', function(){      
+  BoxTop.style.boxShadow = ''; 
 });
 
 
 //-----Bogotá-----//
 Bogota.addEventListener('mouseover', function(){  
   Bogota.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px rgb(0, 195, 255)';
-  
+  BoxTop.style.boxShadow = 'inset 0 0 2px 1px rgba(255, 255, 255, 0.1), 0px 2px 13px 0px #fff';
 });
 Bogota.addEventListener('mouseout', function(){  
-
   Bogota.style.boxShadow = ''
+  
 });
 //-----Lyon-----//
 Lyon.addEventListener('mouseover', function(){
@@ -273,25 +298,31 @@ Quebec.addEventListener('mouseout', function(){
 //-------------[NEW CITY]------------//
 AddNewCity.addEventListener('click', function() {
 
+  const NewCardCity = `NEWCARD${cityName}`
+  document.createElement("div", {id:NewCardCity}); 
+
   //----------Input values-----------//
   var country = CountryInput.value; 
   var city = CityInput.value; 
+  var image1 = CityPicture1.value; 
+  var image2 = CityPicture2.value; 
   //----------Validation Input-----------//
+  //--Input City--//
   if(city === ''){
      if(LanguageSelect.value === '&lang=es'){
       alert(`El campo '${CityInput.placeholder}' es fundamental para agregar una ciudad.`);     
     }
     if(LanguageSelect.value === '&lang=fr'){
-      alert(`Le champ '${CityInput.placeholder}' est essentiel pour pour ajouter une ville.`); 
-    }return
-   
+      alert(`Le champ '${CityInput.placeholder}' est essentiel pour ajouter une ville.`); 
+    }return   
   }
+  //--Input Country--//
   if(country === '' ){
     if(LanguageSelect.value === '&lang=es'){
       alert(`El campo '${CountryInput.placeholder}' es fundamental para agregar una ciudad.`)     
     }
     if(LanguageSelect.value === '&lang=fr'){
-      alert(`Le champ '${CountryInput.placeholder}' est essentiel pour pour ajouter une ville.`); 
+      alert(`Le champ '${CountryInput.placeholder}' est essentiel pour ajouter une ville.`); 
     }return   
     
   }
@@ -300,31 +331,20 @@ AddNewCity.addEventListener('click', function() {
    var countryName = country[0].toLowerCase() + country[1].toLowerCase(); 
    var cityName =  city[0].toUpperCase() + city.slice(1);   
    var newUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}`;
-   
-    
- 
-    
-  
-    
-    
+
      var nameContainer = `div${cityName}`;
      var tempContainer = `temp${cityName}`;
      var iconWeatherContainer = `iconWeather${cityName}`;  
      var humidityContainer = `humidity${cityName}`;
   
-     console.log(newUrl)
-    
-    //Create input
-      
-   
- 
-    NEWCARD.innerHTML=`<div class="flip-card-container" id="${cityName}" style="--hue: 350">
+   //Create input
+     NEWCARD.innerHTML=`<div class="flip-card-container" id="${cityName}" style="--hue: 350">
      <div class="flip-card">
    
        <div class="card-back">
          <figure>
            <div class="img-bg"></div>
-           <img src="img/${cityName}2.jpg">
+           <img src="${image2}">
            <figcaption><i class="fas fa-map-marker-alt" ></i> ${cityName} - ${fullCountry}</figcaption>
          </figure>
    
@@ -340,7 +360,7 @@ AddNewCity.addEventListener('click', function() {
          <!-- only if the image is necessary -->
          <figure>
            <div class="img-bg"></div>
-           <img src="img/${cityName}.jpg">
+           <img src="${image1}">
          </figure>
    
          <button id="${cityName}"></button>
@@ -362,10 +382,15 @@ AddNewCity.addEventListener('click', function() {
    </div>`
 
   
+   getWeather(cityName, newUrl, nameContainer, iconWeatherContainer, tempContainer, humidityContainer);
   
-   if(NEWCARD.innerHTML != ''){
-     getWeather(cityName, newUrl, nameContainer, iconWeatherContainer, tempContainer, humidityContainer);
-     iconWeatherContainer.setAttribute("src", '');  
+   if(NEWCARD.innerHTML != ''){    
+    
+      //Clean input
+      CountryInput.value = ''
+      CityInput.value = ''
+      CityPicture1.value = ''
+      CityPicture2.value = ''       
      console.log(NEWCARD.innerHTML)
      window.location.href ='#'
    }
@@ -432,6 +457,8 @@ function getWeather(nameCity, parameters,  weather, icon, temp, humid){
        textS5.innerHTML = ' Agregar ciudad';  
        CountryInput.placeholder='País'
        CityInput.placeholder = 'Ciudad' 
+       CityPicture1.placeholder = 'Url Imagen frontal'
+       CityPicture2.placeholder = 'Url Imagen posterior'
       
       }
       if(LanguageSelect.value === '&lang=fr'){
@@ -451,6 +478,8 @@ function getWeather(nameCity, parameters,  weather, icon, temp, humid){
         textS5.innerHTML = ' Ajouter une ville';  
         CountryInput.placeholder='Pays'
         CityInput.placeholder = 'Ville' 
+        CityPicture1.placeholder = "Url de l'image avant"
+        CityPicture2.placeholder = "Url de l'image arrière"
       }
      
                     
@@ -469,6 +498,7 @@ var Clock = document.getElementById('clock');
 
 
  Clock.innerHTML =  hours +  ": " + minutes + ": " + seconds;
+ 
 }
 function checkNumber(number){
   if(number < 10) number = "0" + number;  
